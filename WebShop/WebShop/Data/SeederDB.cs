24bf9032b12489720763ced5a573c328045cac22
novakvova <novakvova@gmail>
2023-05-25 18:04:57 +0300

    Complete register

---
+++ WebShop/WebShop/Data/SeederDB.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Constants;
+using WebShop.Data.Entities;
+using WebShop.Data.Entities.Identity;
+
+namespace WebShop.Data
+{
+    public static class SeederDB
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using(var scope=app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                var context = service.GetRequiredService<AppEFContext>();
+                var userNamager = service.GetRequiredService<UserManager<UserEntity>>();
+                var roleNamager = service.GetRequiredService<RoleManager<RoleEntity>>();
+                context.Database.Migrate(); //автоматично запускає міграції на БД
+
+                if(!context.Categories.Any())
+                {
+                    CategoryEntity categoryEntity = new CategoryEntity()
+                    {
+                        Name = "Піжами",
+                        Image = "1.jpg",
+                        Priority = 1,
+                        DateCreated = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc),
+                        Description = "Для нічних прогулок"
+                    };
+                    context.Categories.Add(categoryEntity);
+                    context.SaveChanges();
+                }
+                if(!context.Roles.Any())
+                {
+                    foreach(string name in Roles.All)
+                    {
+                        var role = new RoleEntity
+                        {
+                            Name = name
+                        };
+                        var result = roleNamager.CreateAsync(role).Result;
+                    }
+                }
+                if(!context.Users.Any())
+                {
+                    var user = new UserEntity()
+                    {
+                        FirstName = "Вова",
+                        LastName = "Новак",
+                        Email = "admin@gmail.com",
+                        UserName = "admin@gmail.com"
+                    };
+                    var result = userNamager.CreateAsync(user,"123456").Result;
+                    if(result.Succeeded)
+                    {
+                        result = userNamager.AddToRoleAsync(user, Roles.Admin).Result;
+                    }
+                }
+
+            }
+        }
+    }
+}
--- WebShop/WebShop/Program.cs
+++ WebShop/WebShop/Program.cs
@@ -1,12 +1,26 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using WebShop.Data;
+using WebShop.Data.Entities.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppEFContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("MyConnectionDB")));
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+{
+    options.Stores.MaxLengthForKeys = 128;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+    .AddEntityFrameworkStores<AppEFContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -36,9 +50,10 @@ app.UseStaticFiles(new StaticFileOptions
     RequestPath="/images"
 });
 
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.SeedData();
 
 app.Run();
