41d7acc1acc6332a28c2d616496343431816e5dc
novakvova <novakvova@gmail>
2023-06-17 17:40:24 +0300

    Add tblProductImages

--- WebShop/WebShop/Data/Entities/ProductEntity.cs
+++ WebShop/WebShop/Data/Entities/ProductEntity.cs
@@ -19,5 +19,6 @@ namespace WebShop.Data.Entities
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
         public virtual CategoryEntity Category { get; set; }
+        public virtual ICollection<ProductImageEntity> ProductImages { get; set; }
     }
 }
---
+++ WebShop/WebShop/Data/Entities/ProductImageEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebShop.Data.Entities
+{
+    [Table("tblProductImages")]
+    public class ProductImageEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        [ForeignKey("Product")]
+        public int ? ProductId { get; set; }
+        public virtual ProductEntity Product { get; set; }
+    }
+}
